05b772fc95c58f9686793902e65c62d5f4d4d19e
Hafsa Khan Lodhi <hafsalodhi2023@gmail.com>
2023-10-10 21:05:53 +0500

    Remove cursor

--- js/index.js
+++
@@ -1,7 +0,0 @@
-let cursor = document.querySelector("#cursor");
-let body = document.querySelector("body");
-
-body.addEventListener("mousemove", function(e){
-    cursor.style.left = e.pageX + "px";
-    cursor.style.top = e.pageY + "px";
-});
